ebd334d76ca456f5f8623842a877354b4fd1f2a6
Matthew Elliott <melliott@ccistudios.com>
2011-09-16 17:11:30 -0400

    finished layout of home page

--- public_html/templates/grogs/index.php
+++ public_html/templates/grogs/index.php
@@ -24,14 +24,60 @@
 	
 	<!---<div id="MacPhersonsLogo"><div></div></div>-->
 	
-	<div id="tablecloth"><div id="reservedSign"></div></div>	
+	<!--<div id="tablecloth"><div id="reservedSign"></div></div>-->
 
-	<div id="component"><div class="container">
-		Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-	</div></div>
+	<div class="container"><div id="component">
+	<h2>Welcome to MacPherson’s!</h2>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+<img class="floatleft" src="../../images/default/photo-macphersons.jpg" width="220" height="126">
+<br /><br />Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. <br /><br />Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. <br /><br />Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.<p>
+	</div>
 	
+	<div id="sidebar">
+		<div id="events">
+			
+			<h2>Events</h2>
+			
+			<p>Enjoy your favourite piano tunes every Saturday 6-7pm!</p>
+			
+			<a href="calendar.html">View Calendar</a>
+			
+		</div>
+		<div id="visitUs">
+			
+			<h2>Grogs</h2>
+			
+			<p>Visit our sister restaurant!</p>	
+				
+			<img src="templates/grogs/images/Grogs_Logo.jpg" width="300" height="103" alt="Grogs Logo">			
+			
+		</div>	
+	</div>
+	</div>
 	
+		<div class="clear"></div>
 	
+	<div id="footer">
+		<ul class="menu">
+			<li><a href="#">Menu</a></li>
+			<li><a href="#">Photo Gallery</a></li>
+			<li><a href="#">Calendar</a></li>
+			<li><a href="#">Gift Certificates</a></li>
+			<li><a href="#">Maps/Directions</a></li>
+		</ul>
+		
+		<div class="floatleft">
+			<p>Copyright &copy;<?php echo date('Y') ?> MacPherson's Restaurant.  All Rights Reserved.</p>
+		</div>
+		
+		<div class="floatright">
+			<p>Site by CCI Studios</p>
+		</div>
 
+
+
+	</div>
+	
+	
 </body>
 </html>
