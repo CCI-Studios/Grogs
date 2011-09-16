f95fabebc796e956c12161e13bd48b898569b479
Matthew Elliott <melliott@ccistudios.com>
2011-09-16 11:16:17 -0400

    Beginning of Laying out template

---
+++ public_html/templates/grogs/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	
+	<link rel="stylesheet" href="templates/grogs/css/template.css" type="text/css">
+	
+</head>
+
+<body class="<?php echo $wide ?>">
+		
+	<div id="nav">
+		<div id="navcontainer">
+			<ul class="menu">
+			<li><a href="#">Menu</a></li>
+			<li><a href="#">Photo Gallery</a></li>
+			<li><a href="#">Calendar</a></li>
+			<li><a href="#">Gift Certificates</a></li>
+			<li><a href="#">Maps/Directions</a></li>
+			</ul>
+		</div>
+	</div>
+	
+	<div id="header"><div><div class="container"></div></div></div>	
+	
+	<!---<div id="MacPhersonsLogo"><div></div></div>-->
+	
+	<div id="tablecloth"><div id="reservedSign"></div></div>	
+
+	<div id="component"><div class="container">
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+	</div></div>
+	
+	
+	
+
+</body>
+</html>
