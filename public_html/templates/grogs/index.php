462233e33ded790a3059953100c84cf3083da298
Matthew Elliott <melliott@ccistudios.com>
2011-09-19 17:02:51 -0400

    Modules/Articles Created Needs styling and fixes

--- public_html/templates/grogs/index.php
+++ public_html/templates/grogs/index.php
@@ -9,23 +9,14 @@
 <body class="<?php echo $wide ?>">
 	
 	<div class="container"><div id="header_links">
-		<ul class="menu">
-			<li><a href="#">Grogs</a></li>
-			<li><a href="#">Contact/Hours</a></li>
-		</ul>
+		<jdoc:include type="modules" name="headerlinks" style="xhtml" />		
 	</div></div>
 		
 		<div class="clear"></div>
 		
 	<div id="nav">
-		<div id="navcontainer">
-			<ul class="menu">
-			<li><a href="#">Menu</a></li>
-			<li><a href="#">Photo Gallery</a></li>
-			<li><a href="#">Calendar</a></li>
-			<li><a href="#">Gift Certificates</a></li>
-			<li><a href="#">Maps/Directions</a></li>
-			</ul>
+		<div id="navcontainer">			
+			<jdoc:include type="modules" name="nav" style="xhtml" />
 		</div>
 	</div>
 	
@@ -42,66 +33,23 @@
 	
 	<div class="container"><div id="component">
 		
-<h2>Welcome to MacPherson’s!</h2>
-<p>	Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris mauris eros, adipiscing quis posuere et, vehicula et felis. 				Curabitur ut turpis a felis molestie faucibus. Pellentesque gravida nibh risus. Cras ornare metus at erat feugiat ultrices.
-<br /><br /> Nulla semper tortor et mi euismod convallis. Integer elit turpis, rhoncus ut luctus sed, faucibus eu felis. Phasellus auctor, mi eget ullamcorper posuere, neque arcu mattis elit, ac commodo lorem tortor in dui. Donec tortor massa, vehicula lacinia aliquam quis, pulvinar at felis.<br /><br />
-MacPherson's Restaurant also has:
-
-	<img class="floatleft" src="../../images/default/photo-macphersons.jpg" width="220" height="126">
-
-<ul>
-	<li>Baked Goods</li>
-	<li>Variety Store</li>
-	<li>Fuel</li>
-	<li>Gifts &amp; Cards</li>
-	<li>Movie Rentals</li>
-	<li>Ice</li>
-	<li>Lottery</li>
-	<li>ATM</li>
-</ul>
-</p>
-
-
-
-
+<jdoc:include type="component" />
 
 	</div>
 	
 	<div id="sidebar">
-		<div id="events">
+		
+		<jdoc:include type="modules" name="sidebar" style="xhtml" />
 			
-			<h2>Events</h2>
-			
-			<p>Enjoy your favourite piano tunes every Saturday 6-7pm!</p>
-			
-			<a href="calendar.html">View Calendar</a>
-			
-		</div>
-		<div id="visitUs">
-			
-			<h2>Grogs</h2>
-			
-			<p>Visit our sister restaurant!</p>	
-				
-			<img src="templates/grogs/images/Grogs_Logo.jpg" width="300" height="103" alt="Grogs Logo">			
-			
-		</div>	
 	</div>
 	</div>
 	
 		<div class="clear"></div>
 	
 	<div id="footer">
-		<ul class="menu">
-			<li><a href="#">Home</a></li>
-			<li><a href="#">Grog's</a></li>
-			<li><a href="#">Menu</a></li>
-			<li><a href="#">Photo Gallery</a></li>
-			<li><a href="#">Calendar</a></li>
-			<li><a href="#">Gift Certificates</a></li>
-			<li><a href="#">Maps/Directions</a></li>
-			<li><a href="#">Contact/Hours</a></li>
-		</ul>
+		
+		<jdoc:include type="modules" name="footer" style="xhtml" />
+		
 		
 		<div class="floatleft">
 			<p>Copyright &copy; <?php echo date('Y') ?> MacPherson's Restaurant.  All Rights Reserved.</p>
