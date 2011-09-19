1534aa2adba4e3249e0cc8670598ab8926e3cd8b
Matthew Elliott <melliott@ccistudios.com>
2011-09-19 12:47:54 -0400

    More styling complete - Now inserting modules

--- public_html/templates/grogs/index.php
+++ public_html/templates/grogs/index.php
@@ -7,6 +7,15 @@
 </head>
 
 <body class="<?php echo $wide ?>">
+	
+	<div class="container"><div id="header_links">
+		<ul class="menu">
+			<li><a href="#">Grogs</a></li>
+			<li><a href="#">Contact/Hours</a></li>
+		</ul>
+	</div></div>
+		
+		<div class="clear"></div>
 		
 	<div id="nav">
 		<div id="navcontainer">
@@ -22,15 +31,40 @@
 	
 	<div id="header"><div><div class="container"></div></div></div>	
 	
-	<!---<div id="MacPhersonsLogo"><div></div></div>-->
+	<div id="bg">
+		
+	<div id="head_container">
+		<div id="tableSetup"><img src="templates/grogs/images/tableclothSetup.png" width="853" height="488" alt="TableclothSetup"></div>
+		<div id="reservedIcon"><img src="templates/grogs/images/reserved_icon.png" width="148" height="87" alt="Reserved Icon"></div>
+	</div>
 	
-	<!--<div id="tablecloth"><div id="reservedSign"></div></div>-->
-
+	
+	
 	<div class="container"><div id="component">
-	<h2>Welcome to MacPherson’s!</h2>
-<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-<img class="floatleft" src="../../images/default/photo-macphersons.jpg" width="220" height="126">
-<br /><br />Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. <br /><br />Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. <br /><br />Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.<p>
+		
+<h2>Welcome to MacPherson’s!</h2>
+<p>	Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris mauris eros, adipiscing quis posuere et, vehicula et felis. 				Curabitur ut turpis a felis molestie faucibus. Pellentesque gravida nibh risus. Cras ornare metus at erat feugiat ultrices.
+<br /><br /> Nulla semper tortor et mi euismod convallis. Integer elit turpis, rhoncus ut luctus sed, faucibus eu felis. Phasellus auctor, mi eget ullamcorper posuere, neque arcu mattis elit, ac commodo lorem tortor in dui. Donec tortor massa, vehicula lacinia aliquam quis, pulvinar at felis.<br /><br />
+MacPherson's Restaurant also has:
+
+	<img class="floatleft" src="../../images/default/photo-macphersons.jpg" width="220" height="126">
+
+<ul>
+	<li>Baked Goods</li>
+	<li>Variety Store</li>
+	<li>Fuel</li>
+	<li>Gifts &amp; Cards</li>
+	<li>Movie Rentals</li>
+	<li>Ice</li>
+	<li>Lottery</li>
+	<li>ATM</li>
+</ul>
+</p>
+
+
+
+
+
 	</div>
 	
 	<div id="sidebar">
@@ -59,23 +93,24 @@
 	
 	<div id="footer">
 		<ul class="menu">
+			<li><a href="#">Home</a></li>
+			<li><a href="#">Grog's</a></li>
 			<li><a href="#">Menu</a></li>
 			<li><a href="#">Photo Gallery</a></li>
 			<li><a href="#">Calendar</a></li>
 			<li><a href="#">Gift Certificates</a></li>
 			<li><a href="#">Maps/Directions</a></li>
+			<li><a href="#">Contact/Hours</a></li>
 		</ul>
 		
 		<div class="floatleft">
-			<p>Copyright &copy;<?php echo date('Y') ?> MacPherson's Restaurant.  All Rights Reserved.</p>
+			<p>Copyright &copy; <?php echo date('Y') ?> MacPherson's Restaurant.  All Rights Reserved.</p>
 		</div>
 		
 		<div class="floatright">
-			<p>Site by CCI Studios</p>
+			<p>Site by <a href="http://ccistudios.com" target="_blank">CCI Studios</p>
 		</div>
-
-
-
+	</div>
 	</div>
 	
 	
