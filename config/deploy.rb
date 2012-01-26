6031074608c37a0481763c7d6c1964446a33283d
Matt Elliott <melliott@ccistudios.com>
2012-01-26 09:03:00 -0500

    deploy and staging scripts created

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,14 +3,14 @@ set :stages, %w(staging production)
 set :default_stage, "staging"
 require "capistrano/ext/multistage"
 
-set :application, "APPLICATION_NAME"
+set :application, "MacPherson"
 
 # repository info
-set :repository,  "git@github.com:CCI-Studios/PROJECT.git"
+set :repository,  "git@github.com:CCI-Studios/Grogs.git"
 set :scm, :git
 
 # ssh settings
-set :user, "USERNAME"
+set :user, "staging"
 set :use_sudo, false
 
 # Joomla
